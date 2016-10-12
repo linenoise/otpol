c146390a2e5884daacca0c8bf5a7729365cd6fad
Prelang Builder <builder@prelang.com>
2016-10-12 12:32:11 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Moment' to allow for create, update

--- app/admin/moment.rb
+++ app/admin/moment.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Moment do
+  permit_params :description, :happened_at
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
