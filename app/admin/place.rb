9d2e089e55799af984251cb983e50ea76822b816
Prelang Builder <builder@prelang.com>
2016-10-12 12:31:59 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Place' to allow for create, update

--- app/admin/place.rb
+++ app/admin/place.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Place do
+  permit_params :name, :latitude, :longitude
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
