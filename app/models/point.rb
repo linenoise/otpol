2085b1f69cd228880c0a91b2e848cbbd02c8f640
Danne Stayskal <danne@stayskal.com>
2016-10-13 12:40:02 -0700

    Adding scopes for recent points

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,4 +1,11 @@
 class Point < ActiveRecord::Base
   belongs_to :user
   acts_as_votable 
+
+  validates :description, length: { in: 1..512 }
+
+  scope :recent, -> { 
+  	where("created_at" => 3.days.ago..0.days.ago)
+  }
+
 end
