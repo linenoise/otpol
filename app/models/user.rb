ce57f8d3d467847ae4067c8fde381fd6f588243e
Danne Stayskal <danne@stayskal.com>
2016-10-13 12:39:43 -0700

    Structuring class, adding scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,51 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+
+  ### Authentication
+
+  devise :database_authenticatable, 
+         :registerable, 
+         :recoverable, 
+         :rememberable, 
+         :trackable, 
+         :validatable, 
+         :lockable
+
   attr_accessor :login
+  devise authentication_keys: [:login]
+
+  ### Relation
 
   has_many :points
 
-  # Image attachments
+  ### Scopes
+
+  scope :newcomers, -> { 
+    where("created_at" => 3.days.ago..0.days.ago)
+  }
+  scope :active, -> { 
+    where("last_seen_at" => 1.days.ago..0.days.ago)
+  }
+
+  ### Attributes
+
   has_attached_file :avatar, styles: {
-    thumb: '100x100#',
+    thumb: '50x50#',
     square: '250x250#',
   }
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
-  devise authentication_keys: [:login]
+  ### Validation
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_formatting_of :website, using: :url, allow_nil: true, allow_blank: true
+
+  ### Methods
+
+  def display_name
+    if self.name.empty?
+      '@' + self.username
+    else
+      self.name
+    end
+  end
+
 end
