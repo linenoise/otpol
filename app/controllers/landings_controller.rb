0302b389b8f3bf6b556c14ad1bb5018a648379ae
Danne Stayskal <danne@stayskal.com>
2016-10-13 15:39:22 -0700

    Tabs to spaces

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,8 +1,9 @@
 class LandingsController < ApplicationController
   def index
-	@points = Point.all
-	@active_today = User.active.sort { |x, y| x.display_name <=> y.display_name } 
-	@newcomers = User.newcomers.sort { |x, y| x.display_name <=> y.display_name }
+  	@points = Point.all
+  	@active_today = User.active.sort { |x, y| x.display_name <=> y.display_name } 
+  	@newcomers = User.newcomers.sort { |x, y| x.display_name <=> y.display_name }
+
   	if user_signed_in?
   		render :home
   	else
