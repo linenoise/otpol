3aa77c38c8fee6f80f4cf8f2a53213547621feed
Danne Stayskal <danne@stayskal.com>
2016-10-13 12:31:35 -0700

    Adding lockable and last seen times to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161013151201) do
+ActiveRecord::Schema.define(version: 20161013183124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,10 @@ ActiveRecord::Schema.define(version: 20161013151201) do
     t.text     "affiliations"
     t.string   "place"
     t.string   "name"
+    t.integer  "failed_attempts",                    default: 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "last_seen_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
