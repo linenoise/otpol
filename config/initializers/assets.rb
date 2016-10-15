0297fac69179bf170e1d3e7becb65a243651ac94
Danne Stayskal <danne@stayskal.com>
2016-10-15 16:23:06 -0700

    Fixing precompile call

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 
